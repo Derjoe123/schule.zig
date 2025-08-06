ff9095854a31b87364540746755b530dd5ddf232
Derjoe123 <allotria2@gmail.com>
2025-08-07 01:37:49 +0200

    work

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,8 @@
 var config = struct {
     directory: []const u8 = ".",
     database_file: []const u8 = "schule.sdb", // relative to config.directory
+    git_commit_command: []const u8 = "git add . && git commit -m '{}' && git push --set-upstream origin main",
+    do_git_pull: bool = true,
 }{};
 const table = struct {
     data: []const u8,
@@ -10,13 +12,14 @@ const database = struct {
 };
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}).init;
+    const alloc = gpa.allocator();
     defer {
         if (gpa.deinit() == .leak) {
             std.debug.print("Memory Leak!", .{});
         }
     }
-    const dir_buf = try gpa.allocator().alloc(u8, std.fs.max_path_bytes);
-    defer gpa.allocator().free(dir_buf);
+    const dir_buf = try alloc.alloc(u8, std.fs.max_path_bytes);
+    defer alloc.free(dir_buf);
     const cwd = try std.process.getCwd(dir_buf);
     config.directory = cwd;
     const argv = std.os.argv;
@@ -42,9 +45,25 @@ pub fn main() !void {
     const total_len = config.directory.len + 1 + config.database_file.len;
     var dir_buf_view = dir_buf;
     dir_buf_view.len = total_len;
+    var child = std.process.Child.init(&[_][]const u8{ "echo", "Hello, World\n" }, alloc);
+    child.stdout_behavior = .Pipe;
+    child.stderr_behavior = .Pipe;
+    var stdout: std.ArrayListUnmanaged(u8) = .empty;
+    defer stdout.deinit(alloc);
+    var stderr: std.ArrayListUnmanaged(u8) = .empty;
+    defer stderr.deinit(alloc);
+
+    try child.spawn();
+    try child.collectOutput(alloc, &stdout, &stderr, 1024);
+    _ = try std.io.getStdErr().writer().print("Error: {s}\n", .{stderr.items});
+    _ = try std.io.getStdOut().writer().print("StdOut: {s}\n", .{stdout.items});
+    const term = try child.wait();
+
+    try std.testing.expectEqual(term.Exited, 0);
+    child.cwd = config.directory;
 
     const db = try simpledb.SimpleDB(database, 1024 * 1024 * 1024).init(dir_buf_view);
-    const old_content = try db.get_content(gpa.allocator());
+    const old_content = try db.get_content(alloc);
     defer old_content.deinit();
     std.debug.print("Old Content: {}\n", .{old_content.value});
     const new_content = database{ .tables = &[_]table{ table{ .data = "test" }, table{ .data = "test2" } } };
