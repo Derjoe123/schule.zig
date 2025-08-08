301ecf0fb1558d6f44c9f7b5687cab23477570d2
Derjoe123 <allotria2@gmail.com>
2025-08-08 23:42:34 +0200

    'automatic save'

--- src/shellcmd.zig
+++ src/shellcmd.zig
@@ -42,8 +42,12 @@ pub const shellcmd = struct {
         try child.spawn();
         try child.collectOutput(allocator, &cmd.stdout, &cmd.stderr, max_output_len);
         const term = try child.wait();
-        try std.fmt.format(writer, "\nOut:{s}\n", .{cmd.stdout.items});
-        try std.fmt.format(writer, "Err:{s}\n", .{cmd.stderr.items});
+        if (cmd.stdout.items.len > 0) {
+            try std.fmt.format(writer, "\nOut:{s}\n", .{cmd.stdout.items});
+        }
+        if (cmd.stderr.items.len > 0) {
+            try std.fmt.format(writer, "Err:{s}\n", .{cmd.stderr.items});
+        }
 
         if (term.Exited != 0) {
             std.log.warn("Command exited with non-zero exit code: {}\n", .{term.Exited});
