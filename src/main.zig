360050bc17d78157f0983b88474a44092c96f220
Derjoe123 <allotria2@gmail.com>
2025-08-12 22:52:21 +0200

    'automatic save'

--- src/main.zig
+++ src/main.zig
@@ -62,9 +62,9 @@ pub fn main() !void {
     if (config.do_git_pull) {
         try shellcmd.execute_and_print_output_blocking(writer, config.directory, &[_][]const u8{ "git", "pull" }, alloc, 2048);
     }
-    if (config.do_git_push) {
-        try shellcmd.execute_and_print_output_blocking(writer, config.directory, &[_][]const u8{ "git", "push" }, alloc, 2048);
-    }
+    // if (config.do_git_push) {
+    //     try shellcmd.execute_and_print_output_blocking(writer, config.directory, &[_][]const u8{ "git", "push" }, alloc, 2048);
+    // }
     var db_parser = try db_parser_t.init(dir_buf_view);
     defer db_parser.deinit();
 
